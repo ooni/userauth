e81186c0af451a8e7454b7beb7b44c67e08c48de
Michele Orrù <m@orru.net>
2025-05-18 13:32:45 +0200

    Definition of OONI credentials.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
+use cmz::CMZ;
+use cmz::CMZCred;
+use cmz::{Serialize, Deserialize};
+use curve25519_dalek::ristretto::RistrettoPoint as G;
+use cmz::serde_as;
+use cmz::SerdeScalar;
+use cmz::CMZPrivkey;
+use rand::RngCore;
+use cmz::cmz_privkey_to_pubkey;
+use cmz::CMZPubkey;
+use cmz::CMZMac;
+use group::Group;
+use cmz::CMZCredential;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+
+CMZ! {UserAuthCredential<G>:
+    age, nym_id, rate_id, trust_level }
+
 
 #[cfg(test)]
 mod tests {
