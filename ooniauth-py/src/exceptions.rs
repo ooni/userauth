a5ecf42d378cd2a6837d073c98cec98bec969c3c
Luis Díaz <ldiazn98@gmail.com>
2025-09-18 11:26:31 +0200

    reformat string

--- ooniauth-py/src/exceptions.rs
+++ ooniauth-py/src/exceptions.rs
@@ -5,14 +5,25 @@ use pyo3::PyErr;
 
 
 // Note that the module name must be the name of the pyproject module name to prevent errors,
-// we should create an issue in the pyo3_stub_gen repo about this. The thing is that 
+// we should create an issue in the pyo3_stub_gen repo about this. The thing is that
 // if the value of the module argument here is different of the pyproject one except for
 // _ and -, it confuses itself and thinks that its a different module while writing in
 // the same resulting .pyi, deleting parts of the content
 
 // TODO Probably we need more exception classes for the protocol-related errors
-create_exception!(ooniauth-py, ProtocolError, PyException, "There was an error completing the proocol. Could mean that the user credentials didn't verify, or that the server response didn't verify");
-create_exception!(ooniauth-py, DeserializationFailed, PyException, "There was an error trying to deserialize a binary buffer");
+create_exception!(
+    ooniauth-py,
+    ProtocolError,
+    PyException,
+    "There was an error completing the proocol. Could mean that the user credentials didn't verify,
+    or that the server response didn't verify");
+
+create_exception!(
+    ooniauth-py,
+    DeserializationFailed,
+    PyException,
+    "There was an error trying to deserialize a binary buffer"
+);
 
 
 #[derive(Debug)]
