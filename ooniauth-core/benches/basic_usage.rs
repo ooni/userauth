5cf8aabca448fc64023dc747abb13f499a8623ac
Luis Díaz <ldiazn98@gmail.com>
2025-09-19 13:30:43 +0200

    Added submit bench

--- ooniauth-core/benches/basic_usage.rs
+++ ooniauth-core/benches/basic_usage.rs
@@ -1,7 +1,7 @@
-use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
-use ooniauth_core::{ServerState, UserState};
+use ooniauth_core::{submit, ServerState, UserState};
 use rand::{rngs::ThreadRng, thread_rng};
+use std::hint::black_box;
 
 fn setup() -> (ThreadRng, UserState, ServerState) {
     let mut rng = thread_rng();
@@ -12,12 +12,56 @@ fn setup() -> (ThreadRng, UserState, ServerState) {
     (rng, user, server)
 }
 
+// For now we will only care about server functions, specially handling submit and
+// registration, since those are the most important bottleneck for our backend
 fn bench_registration(c: &mut Criterion) {
-    let (mut rng, user, server) = setup();
+    let (mut rng, user, mut server) = setup();
 
-    let (registration_req , state)= user.request(&mut rng).unwrap();
-    // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    // Create the request
+    let (registration_req, _) = user.request(&mut rng).unwrap();
+    c.bench_function("registration", |b| {
+        b.iter(|| server.open_registration(black_box(registration_req.clone())))
+    });
 }
 
-criterion_group!(benches, bench_registration);
+fn bench_submit(c: &mut Criterion) {
+    let (mut rng, mut user, mut server) = setup();
+
+    // Create the request
+    let (registration_req, reg_state) = user.request(&mut rng).unwrap();
+    let resp = server.open_registration(registration_req).unwrap();
+    user.handle_response(reg_state, resp)
+        .expect("Should handle response properly");
+    let today = server.today();
+    let cc = "VE";
+    let asn = "AS1234";
+
+    let age_range = (today - 30)..(today + 1);
+    let msm_range = 0..100;
+    let ((req, _), nym) = user
+        .submit_request(
+            &mut rng,
+            cc.into(),
+            asn.into(),
+            age_range.clone(),
+            msm_range.clone(),
+        )
+        .unwrap();
+
+    c.bench_function("submit", |b| {
+        b.iter(|| {
+            server.handle_submit(
+                black_box(&mut rng),
+                black_box(req.clone()),
+                black_box(&nym),
+                black_box(cc.into()),
+                black_box(asn.into()),
+                black_box(age_range.clone()),
+                black_box(msm_range.clone()),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, bench_registration, bench_submit);
 criterion_main!(benches);
